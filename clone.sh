c24a579cb2156fbf0cd3fb1a8de09f6c3d82e753
Zach Bryant <zdb282@gmail.com>
2020-12-08 01:15:33 -0800

    bash clone tool

---
+++ clone.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USAGE="Usage: ./clone.sh [-s|--server]=... [-c|--client]=..."
+
+for i in "$@"; do
+case $i in
+    -s=*|--server=*)
+    SERVER="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--client=*)
+    CLIENT="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+	  echo $USAGE
+	  exit 1
+    ;;
+esac
+done
+
+if [[ ! $CLIENT ]] && [[ ! $SERVER ]]; then
+	echo $USAGE
+	exit 1
+fi
+
+if [[ $CLIENT ]]; then
+    git clone --recursive $CLIENT ./client
+fi
+
+if [[ $SERVER ]]; then
+    git clone --recursive $SERVER ./server
+fi
