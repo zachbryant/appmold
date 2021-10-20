1936fa971f87fe96c61bd32f342c0034cea71384
Zach Bryant <zdb282@gmail.com>
2021-10-19 22:36:15 -0700

    fix cloning

--- clone.sh
+++ clone.sh
@@ -14,6 +14,7 @@ APPMOLD_TEMPLATE_REMOTE_NAME="template-upstream-appmold"
 
 git remote add $TEMPLATE_REMOTE_NAME $REPO_URL
 git remote add $APPMOLD_TEMPLATE_REMOTE_NAME git@github.com:zachbryant/appmold.git
-git clone $REPO_URL .
+git fetch
+git pull $TEMPLATE_REMOTE_NAME master --allow-unrelated-histories
 
 echo "If you cloned manually, run: git remote set-url origin <new url>"
